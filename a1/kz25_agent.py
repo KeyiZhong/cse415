30202a854fa8b8d7255969faa7476716f9e4b133
KeyiZhong <kz25@uw.edu>
2019-04-05 13:12:45 -0700

    college name

--- a1/kz25_agent.py
+++ a1/kz25_agent.py
@@ -7,6 +7,7 @@ def introduce():
            "please contact him at kz25@uw.edu. \n"
            "How can I help you?\n")
 
+college_name = null
 
 def respond(the_input):
     wordlist = split(' ', the_input)
@@ -15,14 +16,28 @@ def respond(the_input):
     if match('bye', the_input):
         return 'Goodbye'
     if wordlist[0:1] == ['can', 'you'] or wordlist[0:2]==['could','you']:
-        return
+        return 'Yes I ' + wordlist[0] + ' '.join(wordlist[2:]) + '.'
+    if 'college' in wordlist:
+
+        return collegeSentence()
+
     return uselessSentence();
 
 
-
+7
 def agentName():
     return 'Vandermeer'
 
 
+ueless_count = 0
 def uselessSentence():
-    sentence = ['']
+    sentence = ['Please be more specific',
+                'Being in a college is not easy.',
+                'Someimes you need to be patient.',
+                'Always be hopeful to the future.',
+                ]
+
+
+college_count = 0
+def collegeSentence():
+    sentence = ['It is always hard to get to a college.']
