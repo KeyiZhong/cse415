1205124513216d31c7f06897288b469256029a63
Keyi Zhong <kz25@uw.edu>
2019-04-05 10:48:15 -0700

    INITIAL COMMIT

---
+++ a1/chatbot.py
@@ -0,0 +1,17 @@
+def introduce():
+    print("My name is Dale Vandermeer, and I am a college advisor.")
+    print("I was programmed Keyi Zhong. If you don't like the way I talk")
+    print("Please contact him at kz25@uw.edu.")
+    print("How can I help you?")
+
+
+def respond(theInput):
+
+
+
+def agentName():
+    return 'Vandermeer'
+
+
+def uselessSentence():
+    return['']
