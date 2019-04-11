fd870b3f1c2c681fbc0df83da483774a7bf07772
Keyi Zhong <kz25@uw.edu>
2019-04-10 19:58:37 -0700

    a2 finished

--- a1/kz25_agent.py
+++ a1/kz25_agent.py
@@ -3,8 +3,8 @@ import random
 
 
 def introduce():
-    return("My name is Dale Vandermeer, and I am a college advisor. \n"
-           "I was programmed Keyi Zhong. If you don't like the way I talk \n"
+    return("My name is Dale Vandermeer, I am a college advisor. \n"
+           "I was programmed by Keyi Zhong. If you don't like the way I talk \n"
            "please contact him at kz25@uw.edu. \n"
            "How can I help you?\n")
 
@@ -35,41 +35,53 @@ def respond(the_input):
         return 'Goodbye'
     if match('hi', the_input):
         return 'Hello this is Dale Vandermeer'
+    if 'Good morning' in the_input:
+        return 'Good morning, this is Dale Vandermeer, how can I help you?'
     if wordlist[0] == '':
         return "Please make some noise."
     if 'major' in wordlist:
         return collegeSentence()
     if 'college' in wordlist or 'university' in wordlist and len(wordlist) <= 3:
-        college_list.add_college(' '.join(wordlist))
+        college_list.add_college(stringify(wordlist))
         return collegeSentence()
     if 'college' in wordlist or 'university' in wordlist:
         return 'Please specify the name of university/college.'
     if 'suggestion' in wordlist or 'advice' in wordlist:
         if len(college_list.recall()) == 0:
-            return 'please tell me what college is in your mind'
+            return 'please tell me what college is in your mind.'
         return random.choice(college_list.recall()) + ' would be a good choice!'
     if wordlist[0:2] == ['i', 'am']:
         return "Tell me why and when you are " +\
-              stringify(mapped_wordlist[2:]) + '.'
+              stringify(mapped_wordlist[2:])
     if wpred(wordlist[0]):
-        return "There is no exact " + wordlist[0] + "."
+        return "There is no exact " + wordlist[0]
     if 'because' in wordlist:
         return "Please reconsider this carefully."
     if 'yes' in wordlist:
         return "Are you really sure?"
     if wordlist[0:2] == ['you', 'are']:
         return "I am truly " +\
-              stringify(mapped_wordlist[2:]) + '.'
+              stringify(mapped_wordlist[2:])
     if verbp(wordlist[0]):
         return"I will " + stringify(mapped_wordlist) + ' if I have time.'
     if wordlist[0:3] == ['do', 'you', 'think']:
         return "I don't know, but I am more interested in your opinion."
     if wordlist[0:2] == ['can', 'you'] or wordlist[0:2] == ['could', 'you']:
-        return 'Yes I ' + wordlist[0] + ' ' + stringify(mapped_wordlist[2:]) + '.'
+        return 'Yes I ' + wordlist[0] + ' ' + stringify(mapped_wordlist[2:])
     if 'maybe' in the_input:
-        return 'Please be sure.'
-    if 'you' in mapped_wordlist or 'You' in mapped_wordlist:
-        return stringify(mapped_wordlist) + '.'
+        return random.choice(['Please be sure.',
+                              'Do not hesitate',
+                              'Give a hit',
+                              'Come on'])
+    if 'good' in the_input:
+        return random.choice(['Tell me what is good about it',
+                             'I would like to hear about it',
+                              'What make you think this way',
+                              'Nice!'])
+    if 'name' in wordlist[-1]:
+        return 'My name is Dale Vandermeer'
+    ##if 'you' in mapped_wordlist or 'You' in mapped_wordlist:
+        ##return stringify(mapped_wordlist)
     return uselessSentence()
 
 
@@ -88,7 +100,13 @@ def uselessSentence():
                 'Keep going',
                 'Good for you',
                 'Cool',
-                'I like your jacket'
+                'I like your jacket',
+                'Let me think about it and I will let you know when I figure it out',
+                'You really get to the point where I cannot answer',
+                'Cheer up dude!',
+                'You won',
+                'What do you want',
+                'Please ask a more related question.'
                 ]
     global useless_count
     useless_count += 1
@@ -152,9 +170,9 @@ def verbp(w):
                   'blink', 'crash', 'crunch', 'add'])
 
 
-print(introduce())
-while True:
-    the_input = input()
-    print(respond(the_input))
-    if 'bye' in the_input:
-        break
+#print(introduce())
+#while True:
+#    the_input = input()
+#    print(respond(the_input))
+#    if 'bye' in the_input:
+#        break
